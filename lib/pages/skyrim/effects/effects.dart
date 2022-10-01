9c6bf2a879fbd40b5f0b3df833aa18e021f2f4ad
Gennady Terekhov <ter.ge@yandex.ru>
2022-10-02 00:56:57 +0300

    added data provider

--- lib/pages/skyrim/effects/effects.dart
+++ lib/pages/skyrim/effects/effects.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:path_provider/path_provider.dart';
+import 'package:path_provider_platform_interface/path_provider_platform_interface.dart';
+import 'package:plugin_platform_interface/plugin_platform_interface.dart';
+import 'package:the_elder_scrolls_alchemy_client/data/data.dart';
+import 'dart:async';
+import 'dart:io';
+import 'package:the_elder_scrolls_alchemy_client/models/effect.dart';
 
 class EffectsPage extends StatefulWidget {
   const EffectsPage({Key? key}) : super(key: key);
@@ -10,6 +18,11 @@ class EffectsPage extends StatefulWidget {
 class _EffectsPageState extends State<EffectsPage> {
   @override
   Widget build(BuildContext context) {
+    const id = '000ae723';
+    Effect effect = DataProvider.getEffectById(id);
+
+    String dataFromJson = '${effect.id}  ${effect.name} ';
+
     return Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
@@ -18,7 +31,8 @@ class _EffectsPageState extends State<EffectsPage> {
             text: TextSpan(
               children: [
                 TextSpan(
-                    text: '''Effects page''',
+                    // text: '''Effects page''',
+                    text: dataFromJson,
                     style: Theme.of(context).textTheme.headline5),
               ],
             ),
