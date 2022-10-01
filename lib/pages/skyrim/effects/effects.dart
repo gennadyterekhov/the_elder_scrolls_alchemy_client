cd72d91bfd98c5778f4bd4d9884442681b66807d
Gennady Terekhov <ter.ge@yandex.ru>
2022-10-02 00:58:09 +0300

    upd

--- lib/pages/skyrim/effects/effects.dart
+++ lib/pages/skyrim/effects/effects.dart
@@ -1,11 +1,5 @@
 import 'package:flutter/material.dart';
-import 'dart:convert';
-import 'package:path_provider/path_provider.dart';
-import 'package:path_provider_platform_interface/path_provider_platform_interface.dart';
-import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 import 'package:the_elder_scrolls_alchemy_client/data/data.dart';
-import 'dart:async';
-import 'dart:io';
 import 'package:the_elder_scrolls_alchemy_client/models/effect.dart';
 
 class EffectsPage extends StatefulWidget {
