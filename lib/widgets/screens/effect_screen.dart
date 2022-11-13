483c93b0cf55a8995aa5467c3aeb80f8ee7b3cee
Gennady Terekhov <ter.ge@yandex.ru>
2022-11-13 20:25:55 +0300

    ci-40

--- lib/widgets/screens/effect_screen.dart
+++ lib/widgets/screens/effect_screen.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:the_elder_scrolls_alchemy_client/widgets/screens/custom_screen.dart';
-import 'package:the_elder_scrolls_alchemy_client/models/effect.dart';
+import 'package:the_elder_scrolls_alchemy_client/data/effect_resource.dart';
 import 'package:the_elder_scrolls_alchemy_client/widgets/pages/effects/effect.dart';
-import 'package:the_elder_scrolls_alchemy_client/widgets/pages/effects/effects.dart';
+import 'package:the_elder_scrolls_alchemy_client/widgets/screens/custom_screen.dart';
 
 class EffectScreen extends StatelessWidget {
-  const EffectScreen({Key? key, required this.effect}) : super(key: key);
-  final Effect effect;
+  const EffectScreen({Key? key, required this.effectName, required this.gameName}) : super(key: key);
+  final String effectName;
+  final String gameName;
 
   @override
   Widget build(BuildContext context) {
-    return CustomScreen(pageWidget: EffectPage(effect: this.effect));
+    final effect = EffectResource(gameName).getEffectByName(effectName);
+    return CustomScreen(pageWidget: EffectPage(effect: effect));
   }
 }
