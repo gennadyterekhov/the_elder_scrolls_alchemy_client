45fff8403a158413bd6449ea6e9bb99ed59fc7bc
Gennady Terekhov <ter.ge@yandex.ru>
2023-01-12 18:23:25 +0300

    upd

--- lib/data/provider.dart
+++ lib/data/provider.dart
@@ -1,10 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:the_elder_scrolls_alchemy_client/data/data_source.dart';
 
-final globalGamesMapProvider = Provider((ref) {
-  return DataSource.getMap();
-});
-
 final globalIsSearchShownStateProvider = StateProvider((ref) {
   return true;
 });
@@ -12,11 +8,3 @@ final globalIsSearchShownStateProvider = StateProvider((ref) {
 final globalChosenTabIndexStateProvider = StateProvider((ref) {
   return 1;
 });
-
-final globalGameNameStateNotifierProvider = StateNotifierProvider<GlobalGameNameNotifier, String>((ref) {
-  return GlobalGameNameNotifier();
-});
-
-class GlobalGameNameNotifier extends StateNotifier<String> {
-  GlobalGameNameNotifier() : super('sdf');
-}
