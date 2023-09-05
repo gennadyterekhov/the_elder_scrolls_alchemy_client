cf3d9897f7a1b2bc2694cfbb761f33ad945e0f33
Gennady Terekhov <ter.ge@yandex.ru>
2023-09-05 22:22:31 +0300

    d

--- lib/di_container.dart
+++ lib/di_container.dart
@@ -1,8 +1,18 @@
 import 'package:flutter_simple_dependency_injection/injector.dart';
+import 'package:the_elder_scrolls_alchemy_client/app.dart';
+import 'package:the_elder_scrolls_alchemy_client/layers/business_logic/services/settings_manager.dart';
 
 class DependencyInjectionContainer {
-
   Injector initialise(Injector injector) {
+    injector.map<SettingsManager>((injector) => SettingsManager(), isSingleton: true);
+
+    injector.map<TheElderScrollsAlchemyClientApp>(
+      (injector) => TheElderScrollsAlchemyClientApp(
+        settingsManager: injector.get<SettingsManager>(),
+      ),
+      isSingleton: true,
+    );
+
     // injector.map<Logger>((i) => Logger(), isSingleton: true);
     // injector.map<String>((i) => "https://api.com/", key: "apiUrl");
     // injector.map<SomeService>(
