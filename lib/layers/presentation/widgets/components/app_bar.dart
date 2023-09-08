ca748e940504430ddb6a2568c29d301aca6aa765
Gennady Terekhov <ter.ge@yandex.ru>
2023-09-09 00:44:14 +0300

    d

--- lib/layers/presentation/widgets/components/app_bar.dart
+++ lib/layers/presentation/widgets/components/app_bar.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:the_elder_scrolls_alchemy_client/layers/data/resources/constant.dart';
-import 'package:the_elder_scrolls_alchemy_client/layers/data/resources/data_resource.dart';
-import 'package:the_elder_scrolls_alchemy_client/extensions/capitalize.dart';
 import 'package:the_elder_scrolls_alchemy_client/layers/state_management/app_state.dart';
-import 'package:the_elder_scrolls_alchemy_client/main.dart';
-import 'package:the_elder_scrolls_alchemy_client/layers/state_management/state/search_field_toggle.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class AlchemyAppBar extends StatelessWidget implements PreferredSizeWidget {
@@ -16,40 +12,37 @@ class AlchemyAppBar extends StatelessWidget implements PreferredSizeWidget {
   Size get preferredSize => Size(20.0, 50.0);
 
   @override
-  AppBar build(
+  Widget build(
     BuildContext context,
   ) {
-    debugPrint('inside AlchemyAppBar build. this.gameName:');
-    debugPrint(this.gameName);
-
     final homeLink = InkWell(
       onTap: () => context.read<AppState>().moveToHome(),
       child: Text(Constant.getGameNameForPresentation(gameName)),
     );
 
-    // final searchFieldToggle = Provider.of<SearchFieldToggle>(context);
-    // final isSearchVisible = searchFieldToggle.isSearchFieldShown;
+    final blocBuilder = BlocBuilder<AppState, Map<String, dynamic>>(
+      builder: (context, state) {
+        final toggleSearchInkWell = InkWell(
+          child: Wrap(children: [
+            state['isSearchVisible'] ? const Icon(Icons.visibility_off) : const Icon(Icons.visibility),
+            Text(' ${AppLocalizations.of(context)!.appBarSearchToggle}'),
+          ]),
+          onTap: () => context.read<AppState>().toggleSearch(),
+        );
 
-    final isSearchVisible = false;
-
-    final toggleSearchInkWell = InkWell(
-      child: Wrap(children: [
-        isSearchVisible ? const Icon(Icons.visibility_off) : const Icon(Icons.visibility),
-        // Text(' ' + AppLocalizations.of(context)!.appBarSearchToggle),
-        Text(' asdfasd'),
-      ]),
-      // onTap: () => searchFieldToggle.isSearchFieldShown = !isSearchVisible,
+        final titleLine = Wrap(
+          alignment: WrapAlignment.spaceEvenly,
+          spacing: 32,
+          runAlignment: WrapAlignment.spaceEvenly,
+          children: [homeLink, toggleSearchInkWell],
+        );
+        return AppBar(
+          automaticallyImplyLeading: true,
+          title: titleLine,
+        );
+      },
     );
 
-    final titleLine = Wrap(
-      alignment: WrapAlignment.spaceEvenly,
-      spacing: 32,
-      runAlignment: WrapAlignment.spaceEvenly,
-      children: [homeLink, toggleSearchInkWell],
-    );
-    return AppBar(
-      automaticallyImplyLeading: true,
-      title: titleLine,
-    );
+    return blocBuilder;
   }
 }
