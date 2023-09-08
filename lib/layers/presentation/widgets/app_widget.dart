608d9802e17c307613b9f250e1467e6b64720bb3
Gennady Terekhov <ter.ge@yandex.ru>
2023-09-09 00:16:35 +0300

    d

--- lib/layers/presentation/widgets/app_widget.dart
+++ lib/layers/presentation/widgets/app_widget.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:provider/provider.dart';
 import 'package:the_elder_scrolls_alchemy_client/layers/data/resources/constant.dart';
 import 'package:the_elder_scrolls_alchemy_client/layers/presentation/widgets/material_app_with_locale.dart';
 import 'package:the_elder_scrolls_alchemy_client/layers/presentation/widgets/screens/effect_screen.dart';
@@ -9,11 +8,7 @@ import 'package:the_elder_scrolls_alchemy_client/layers/presentation/widgets/scr
 import 'package:the_elder_scrolls_alchemy_client/layers/presentation/widgets/screens/ingredient_screen.dart';
 import 'package:the_elder_scrolls_alchemy_client/layers/presentation/widgets/screens/ingredients_screen.dart';
 import 'package:the_elder_scrolls_alchemy_client/layers/state_management/app_state.dart';
-import 'package:the_elder_scrolls_alchemy_client/router.dart';
-import 'package:the_elder_scrolls_alchemy_client/layers/state_management/state/search_field_toggle.dart';
 import 'package:the_elder_scrolls_alchemy_client/layers/presentation/widgets/screens/error_screen.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class AppWidget extends StatefulWidget {
   AppWidget({Key? key, required this.languageCode, required this.gameName}) : super(key: key);
@@ -31,60 +26,29 @@ class _AppWidgetState extends State<AppWidget> {
   }
 
   Widget buildWidgetBasedOnState(BuildContext context) {
-    debugPrint(' ');
-
-    debugPrint('in buildWidgetBasedOnState');
-    String chosenTab = context.read<AppState>().get()['chosenTab'];
-    debugPrint('chosenTab');
-    debugPrint(chosenTab);
-
     final blocBuilder = BlocBuilder<AppState, Map<String, dynamic>>(
       builder: (context, state) {
-        debugPrint(' ');
-
-        debugPrint('in buildWidgetBasedOnState giantbb!!');
-
-        chosenTab = state['chosenTab'];
-        debugPrint('chosenTab from state: ${chosenTab}');
+        final chosenTab = state['chosenTab'];
 
         if (chosenTab == Constant.tabHome) {
-          final chosenGameName = context.read<AppState>().get()['gameName'];
-
-          debugPrint('going to render HomeScreen with gameName:');
-          debugPrint(chosenGameName);
-          final hs = HomeScreen(gameName: state['gameName']);
-          // final bb = BlocBuilder<AppState, Map<String, dynamic>>(
-          //   builder: (context, state) {
-          //     return HomeScreen(gameName: state['gameName']);
-          //   },
-          // );
-          return hs;
+          return HomeScreen(gameName: state['gameName']);
         }
 
         if (chosenTab == Constant.tabEffects) {
-          final chosenEffectName = context.read<AppState>().get()['chosenEffectName'];
-          if (chosenEffectName != '') {
-            return EffectScreen(gameName: widget.gameName, effectName: chosenEffectName);
+          if (state['chosenEffectName'] != '') {
+            return EffectScreen(gameName: state['gameName'], effectName: state['chosenEffectName']);
           }
           return EffectsScreen(gameName: state['gameName']);
-
-
         }
 
         if (chosenTab == Constant.tabIngredients) {
-          final chosenIngredientName = context.read<AppState>().get()['chosenIngredientName'];
-          if (chosenIngredientName != '') {
-            return IngredientScreen(gameName: widget.gameName, ingredientName: chosenIngredientName);
+          if (state['chosenIngredientName'] != '') {
+            return IngredientScreen(gameName: state['gameName'], ingredientName: state['chosenIngredientName']);
           }
           return IngredientsScreen(gameName: state['gameName']);
         }
 
-        debugPrint('if the code is here means chosenTab is wrong');
-
-        chosenTab = state['chosenTab'];
-        debugPrint('chosenTab from state: ${chosenTab}');
-
-        return ErrorScreen(error: {'name': 'hello'});
+        return const ErrorScreen(error: {'error': 'Route not found'});
       },
     );
 
